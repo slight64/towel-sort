093ac3586f8913fb52ced903cfb570a920bbeb6f
slight64 <slight1989@gmail.com>
2021-03-07 01:08:51 +0400

    feat: domplete function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+        return matrix === undefined ? [] : matrix.map((e,i) => i % 2 === 1? e.reverse() : e).flat()
+    }
